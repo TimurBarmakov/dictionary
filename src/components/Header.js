569dd54f9dbcf9fe5d5db440e8c655d56adccc24
Timur Barmakov <tbarmakov@bk.ru>
2024-05-10 16:35:26 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import BookImage from '../assets/images/book.svg';
+import MoonImage from '../assets/images/moon.svg';
+
+
+const Header = () => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [fontStyle, setFontStyle] = useState('serif');
+
+    useEffect(() => {
+        document.body.classList.toggle('dark-theme', isDarkTheme);
+        document.body.classList.toggle('light-theme', !isDarkTheme);
+    }, [isDarkTheme]);
+
+    useEffect(() => {
+        document.body.style.fontFamily = fontStyle;
+    }, [fontStyle]);
+
+    const handleThemeChange = () => {
+        setIsDarkTheme(!isDarkTheme);
+    };
+
+    const handleFontChange = (event) => {
+        setFontStyle(event.target.value);
+    };
+
+    return (
+        <header className="header">
+            <div className="container">
+                <div className='logoContainer'>
+                    <img src={BookImage} alt="Book Logo" />
+                </div>
+                <div className="settingsContainer">
+                    <select id="fontStyleSelect" className="font-select" value={fontStyle} onChange={handleFontChange}>
+                        <option value="serif">Serif</option>
+                        <option value="sans-serif">Sans-Serif</option>
+                        <option value="monospace">Monospace</option>
+                    </select>
+                    <div className="themeToggle">
+                        <label className="switch">
+                            <input type="checkbox" id="themeToggleCheckbox" checked={isDarkTheme} onChange={handleThemeChange} />
+                            <span className="slider"></span>
+                        </label>
+                        <div className="themeImage">
+                            <img src={MoonImage} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/MainBlock.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import SearchImage from '../assets/images/search.svg';
+import styles from './MainBlock.module.css';
+
+
+const MainBlock = () => {
+    const [word, setWord] = useState('');
+    const [wordInfo, setWordInfo] = useState(null);
+    const [error, setError] = useState(''); 
+    const [wiktionaryUrl, setWiktionaryUrl] = useState(''); 
+
+    const fetchWordInfo = async () => {
+        if (!word) return; 
+
+        try {
+            const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+            setWiktionaryUrl(`https://en.wiktionary.org/wiki/${word}`); 
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Word not found');
+            }
+            const data = await response.json();
+            setWordInfo(data);
+            setError('');
+        } catch (e) {
+            setError(e.message);
+            setWordInfo(null);
+        }
+    };
+
+    const handleChange = (event) => {
+        setWord(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetchWordInfo();
+    };
+
+    const playAudio = (audioUrl) => {
+        const correctAudioUrl = audioUrl.startsWith('http') ? audioUrl : `https:${audioUrl}`;
+        const audio = new Audio(correctAudioUrl);
+        audio.play().catch(e => console.error("Ошибка воспроизведения аудио:", e));
+    };
+    
+
+
+    return (
+
+        <div className={styles.bodyTransitions}>
+            <div className={styles.container}>
+            <form onSubmit={handleSubmit} className={styles.searchForm}>
+                <input
+                    type="text"
+                    className={styles.inputField}
+                    value={word}
+                    onChange={handleChange}
+                    placeholder="Enter a word..."
+                />
+                <button type="submit" className={styles.searchButton}>
+                    <img type="submit" src={SearchImage} alt="Search" />
+                </button>
+            </form>
+            {error && <p>Error: {error}</p>}
+            {wordInfo && (
+                 <div className="word-details">
+                 {wordInfo.map((info, index) => (
+                     <div key={index} className={styles.wordCard}>
+                         <h2 className={styles.bigBoldText}>{info.word}</h2>
+                         {info.phonetics && info.phonetics.length > 0 && (
+                            <div className={styles.phonetic}>
+                                <span>{info.phonetics[0].text}</span>
+                                {info.phonetics[0].audio && (
+                                <button onClick={() => playAudio(info.phonetics[0].audio)} className={styles.audioButton}>
+                                    🔊
+                                </button>
+                                
+                                )}
+                            </div>
+                        )}
+                        
+                        {info.meanings.map((meaning, index) => (
+                            <div key={index}>
+                                <div className={styles.nounContainer}>
+                                    <p>{meaning.partOfSpeech}</p>
+                                    <hr />
+                                </div>
+                                <h3>Meaning</h3>
+                                <ul className={styles.definitionList}>
+                                    {meaning.definitions.map((def, idx) => (
+                                        <li key={idx}>
+                                            {def.definition}
+                                            {def.example && (
+                                                <p className={styles.exampleStyle}>"{def.example}"</p>
+                                            )}
+                                        </li>
+                                ))}
+                                </ul>
+                                {meaning.synonyms && meaning.synonyms.length > 0 && (
+                                <div className={styles.synonymContainer}>
+                                    <h4>Synonyms</h4>
+                                    <p>{meaning.synonyms.join(', ')}</p>
+                                </div>
+                                
+                                
+                            )}
+                            
+                            </div>
+                            
+                        ))}
+                        
+                     </div>
+                 ))}
+                 <hr />
+                 <div className={styles.urlContainer}>
+                    <p>Source: <a href={wiktionaryUrl} target="_blank" rel="noopener noreferrer">{wiktionaryUrl}</a></p>
+                </div>
+             </div>
+            )}
+            </div>
+        </div>
+    );
+};
+
+export default MainBlock;
